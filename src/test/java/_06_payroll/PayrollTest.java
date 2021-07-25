87ddd3466424cbfde104dc2b8b30066ebe690397
lonedragon513 <lonedragon513@192.168.3.195>
2021-07-24 17:55:36 -0700

    Initial commit

--- src/test/java/_06_payroll/PayrollTest.java
+++ src/test/java/_06_payroll/PayrollTest.java
@@ -11,28 +11,43 @@ class PayrollTest {
     @Test
     void itShouldCalculatePaycheck() {
         //given
-
+    	double a = 1.1;
+    	int b = 4;
+    	double expected = 4.4;
+    	
         //when
-
+    	double actual = Payroll.calculatePaycheck(a, b);
         //then
+    	assertEquals(expected, actual);
+
     }
 
     @Test
     void itShouldCalculateMileageReimbursement() {
         //given
-
+int a = 8;
+double e = 4.6;
         //when
 
+double actual = Payroll.calculateMileageReimbursement(a);
+
         //then
+assertEquals(e, actual);
+
     }
 
     @Test
     void itShouldCreateOfferLetter() {
         //given
-
+String a ="a";
+double b = 1.1;
+String e = "Hello a, We are pleased to offer you an hourly wage of 1.1";
         //when
+String actual = Payroll.createOfferLetter(a,b);
 
         //then
+assertEquals(e, actual);
+
     }
 
 }
